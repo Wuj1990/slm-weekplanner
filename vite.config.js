b92c4833a867c083d4e4ac5008bb4d8aa7d8d102
Jeroen Wuyts <wuj@slm.be>
2026-09-23 18:45:01 +0200

    Herstel stabiele vite config voor succesvolle build

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    target: 'esnext',
-    minify: 'esbuild', // Gebruikt de ingebouwde snelle minifier van Vite
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('firebase')) {
-              return 'firebase-vendor'
-            }
-            return 'vendor'
-          }
-        }
-      }
-    }
   },
 })
