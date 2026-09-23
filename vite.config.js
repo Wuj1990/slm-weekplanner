9d3489312fda91f83cf672dce5efd042171fd3f5
Jeroen Wuyts <wuj@slm.be>
2026-09-23 18:25:52 +0200

    Optimaliseer vite.config.js voor productie-builds en prestaties

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -5,6 +6,26 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    target: 'esnext',
+    minify: 'terser',
     chunkSizeWarningLimit: 1000,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) {
+              return 'firebase-vendor'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
   },
 })
